1dbfa5cb44e3a70ffdf8f70ae8014592c650f366
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-04 00:02:41 +0100

    Servidor configurado com os metodos config, routes e start

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
 import express from 'express';
 
-//Class Server foi criado...
-class Server{
+class Server {
 
-    //Contrutor
-    constructor(){
+    private app: express.Application;
+
+    constructor() {
+        //Inicializando a applicacao exprecess.
+        this.app = express();
+        this.config(); //setando configuracoes.
+    }
+
+
+    // Configuração do servidor
+    config() {
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    // Onde serão definido as rotas
+    routes() {
 
     }
+
+    //Inicialização do servidor
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log(`Running on port: ${this.app.get('port')}`)
+        });
+    }
 }
+
+const server = new Server();
+server.start();

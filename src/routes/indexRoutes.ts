754fbb8de758dd0797f87537b2bea161931165d0
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-04 09:14:22 +0100

    Rota incial colada em rotas

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -10,7 +10,7 @@ class IndexRoutes {
     }
 
     routers() {
-        this.router.get(('/'), (req, res) => res.send('Hello, world from REST API with Node!!'));
+        this.router.get(('/'), (req, res) => res.send('Hello, world from REST API with Node!! \nAPI: /api/post/'));
     }
 }
 

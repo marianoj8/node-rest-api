cf4ef00195a0525009c2897d4c24f52c2db2faec
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-04 11:03:39 +0100

    Esquema do POst foi definido e atribuido na constante PostSchema

--- src/shared/model/Post.ts
+++ src/shared/model/Post.ts
@@ -1,1 +1,10 @@
 import { Schema, model } from 'mongoose';
+
+const PostSchema = new Schema({
+    titulo: { type: String, required: true },
+    url: { type: String, required: true, unique: true, lowercase: true},
+    conteudo: { type: String, required: true },
+    imagem: String,
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: Date
+});

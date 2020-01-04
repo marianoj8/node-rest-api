27705db05fe1174bc2e940891dd9b39ec2715c6c
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-04 21:49:43 +0100

    Subistituindo campos createdAt e updatedAt pelo objecto {timestamps:true}

--- src/shared/model/Post.ts
+++ src/shared/model/Post.ts
@@ -4,9 +4,9 @@ const PostSchema = new Schema({
     titulo: { type: String, required: true },
     url: { type: String, required: true, unique: true, lowercase: true },
     conteudo: { type: String, required: true },
-    imagem: String,
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: Date
+    imagem: String
+}, {
+    timestamps: true // é o mesmo que createdAt e updatedAt no objeto acima...
 });
 
 export default model('Post', PostSchema);

2f977210fbe64f5a3a747256a3c7ab7d1cd0a933
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-04 11:05:07 +0100

    Exportando o modelo Post

--- src/shared/model/Post.ts
+++ src/shared/model/Post.ts
@@ -2,9 +2,11 @@ import { Schema, model } from 'mongoose';
 
 const PostSchema = new Schema({
     titulo: { type: String, required: true },
-    url: { type: String, required: true, unique: true, lowercase: true},
+    url: { type: String, required: true, unique: true, lowercase: true },
     conteudo: { type: String, required: true },
     imagem: String,
     createdAt: { type: Date, default: Date.now },
     updatedAt: Date
 });
+
+export default model('Post', PostSchema);

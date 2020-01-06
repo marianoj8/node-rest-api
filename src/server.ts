f0d21310fd31cad807342568b0771e727c96fabd
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-06 17:33:07 +0100

    arquivo server.ts foi refatorado

--- src/server.ts
+++ src/server.ts
@@ -1,63 +1,6 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import compression from 'compression';
-import cors from 'cors';
+import app from './app';
 
-import indexRoutes from './routes/indexRoutes';
-import postRoutes from './routes/PostsRoutes';
-import userRoutes from './routes/UserRoutes';
-
-class Server {
-
-    private app: express.Application;
-
-    constructor() {
-        //Inicializando a applicacao exprecess.
-        this.app = express();
-        this.config(); //setando configuracoes.
-        this.routes(); // setando rotas.
-    }
-
-
-    // Configuração do servidor
-    config() {
-        //Connecting to MongoDB
-        const MONGO_URI = 'mongodb://localhost/noderestapi';
-        mongoose.set('useFindAndModify', false);
-        mongoose.connect(MONGO_URI || process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        });
-
-        // Settings
-        this.app.set('port', process.env.PORT || 3000);
-
-        //Middlewares
-        this.app.use(morgan('dev'));
-        this.app.use(express.json()); //dando suport ao formato JSON
-        this.app.use(express.urlencoded({ extended: false })); // Validacao e suporte a formularios
-        this.app.use(helmet());
-        this.app.use(compression());
-        this.app.use(cors()); //Removendo o cors
-    }
-
-    // Onde serão definido as rotas
-    routes() {
-        this.app.use(indexRoutes);
-        this.app.use('/api/posts', postRoutes);
-        this.app.use('/api/users', userRoutes);
-    }
-
-    //Inicialização do servidor
-    start() {
-        this.app.listen(this.app.get('port'), () => {
-            console.log(`Running on port: ${this.app.get('port')}`)
-        });
-    }
-}
-
-const server = new Server();
-server.start();
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+    console.log(`Running on port: ${app.get('port')}`);
+});

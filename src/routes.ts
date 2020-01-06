cc46f9477d2365011a128ca7ad88a3d0dcaddf13
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-06 17:32:25 +0100

    arquivo routes.ts foi criado e tadas rotas configuradas

---
+++ src/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { check, validationResult } from 'express-validator';
+
+import UserContoller from './controllers/UserContoller';
+import PostsController from './controllers/PostsController';
+
+const routes = Router();
+
+//Todas as rotas do controlador UserController
+routes.get('/user/:username', UserContoller.getByUrl);
+routes.get('/user', UserContoller.list);
+routes.post('/user', [], UserContoller.store);
+routes.put('/user/:username', UserContoller.update);
+routes.delete('/user/:username', UserContoller.delete);
+routes.delete('/user', UserContoller.deleteByEntity);
+
+//Todas as rotas dos Posts
+routes.get('/post/:url', PostsController.getByUrl);
+routes.get('/post', PostsController.list);
+routes.post('/post', PostsController.store);
+routes.put('/post/:url', PostsController.update);
+routes.delete('/post/:url', PostsController.delete);
+routes.delete('/post', PostsController.deleteByEntity);
+
+export default routes;
